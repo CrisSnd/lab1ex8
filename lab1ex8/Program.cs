49593aea65fee341c944bc1d380046faeabda4f3
Cristi Sandu <sandu.cristi@yahoo.com>
2023-06-30 16:17:28 +0300

    Add project files.

---
+++ lab1ex8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Program care interschimba valoarea a doua variabile intregi introduce de la tastatura. */
+
+            int x;
+            int y;
+
+            Console.WriteLine("Introduceti prima variabila:");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti a doua variabila:");
+            y=int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine();
+
+
+            Console.Write("Inainte de schimbarea variabilelor:" + "x = " + x + ";" + " y = " + y+ ";");
+            x = x * y;      
+            y = x / y;     
+            x = x / y;    
+            Console.Write("\n"+ "Dupa schimbarea variabilelor:" +"      "+ "x = " + x + ";" + " y = " + y+ ".");
+           
+
+            Console.WriteLine();
+        }
+    }
+}
